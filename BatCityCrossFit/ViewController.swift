59b4b2c1ca5b01a31c3d3748955789cdb5c0357c
Tommy Do <tomdo98@tommys-mbp.attlocal.net>
2017-09-16 13:55:42 -0500

    Initial Commit

---
+++ BatCityCrossFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BatCityCrossFit
+//
+//  Created by Tommy Do on 9/16/17.
+//  Copyright © 2017 Tommy Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
